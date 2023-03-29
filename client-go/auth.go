c88d18fcca55ff8acca3fdf637fbd7b47fb67d77
xjayleex <aprilmagnificat@gmail.com>
2023-03-29 16:18:21 +0900

    Getting AccessToken and test it

--- client-go/auth.go
+++ client-go/auth.go
@@ -10,8 +10,10 @@ import (
 )
 
 const (
-	AuthLoginUrl    = "/api/v1/gateway/authentication/user/login"
-	RefreshTokenKey = "refreshToken"
+	AuthLoginEndpoint       = "/api/v1/gateway/authentication/user/login"
+	AuthAccessTokenEndpoint = "/api/v1/gateway/authentication/user/refreshUser"
+	RefreshTokenKey         = "refreshToken"
+	AccessTokenKey          = "accessToken"
 )
 
 type RefreshToken string
@@ -33,19 +35,26 @@ func NewRefreshTokenRequest(baseURL, uid, password string) *RefreshTokenRequest 
 }
 
 func GetRefreshToken(template *RefreshTokenRequest) (RefreshToken, error) {
-	url := fmt.Sprintf("%s%s%s", HttpScheme, template.baseURL, AuthLoginUrl)
+	endpoint := fmt.Sprintf("%s%s%s", HttpScheme, template.baseURL, AuthLoginEndpoint)
 	marshalled, err := json.Marshal(template)
 	if err != nil {
 		return "", errors.New("unable to marshal request body")
 	}
-	req, err := http.NewRequest("POST", url, bytes.NewReader(marshalled))
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewReader(marshalled))
 	if err != nil {
 		return "", fmt.Errorf("unable to make http request with given request template : %v", err)
 	}
 	// TODO : required http header must be added here.
+	req.Header.Set("Content-Type", "application/json")
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", errors.New("unable to get refresh token")
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return "", errors.New("failed on create refresh token")
 	}
 
 	defer resp.Body.Close()
@@ -61,11 +70,68 @@ func GetRefreshToken(template *RefreshTokenRequest) (RefreshToken, error) {
 		return "", errors.New("unable to unmarshal response body")
 	}
 
-	token, ok := data["refreshToken"]
+	token, ok := data[RefreshTokenKey]
 
 	if !ok {
-		return "", errors.New("failed on get refreshToken from response body")
+		return "", errors.New("key for refresh token does not exist in response body")
 	}
 	return RefreshToken(fmt.Sprint(token)), nil
+}
 
+type AccessToken string
+
+type AccessTokenRequest struct {
+	baseURL      string
+	Uid          string       `json:"id"`
+	RefreshToken RefreshToken `json:"refreshToken"`
+}
+
+func NewAccessTokenRequest(baseURL, uid string, refreshToken *RefreshToken) *AccessTokenRequest {
+	return &AccessTokenRequest{
+		baseURL:      baseURL,
+		Uid:          uid,
+		RefreshToken: RefreshTokenKey,
+	}
+}
+
+func GetAccessToken(template *AccessTokenRequest) (AccessToken, error) {
+	endpoint := fmt.Sprintf("%s%s%s", HttpScheme, template.baseURL, AuthAccessTokenEndpoint)
+	marshalled, err := json.Marshal(template)
+	if err != nil {
+		return "", errors.New("unable to marshal request body")
+	}
+	req, err := http.NewRequest("POST", endpoint, bytes.NewReader(marshalled))
+	if err != nil {
+		return "", fmt.Errorf("unable to make http request with given request template : %v", err)
+	}
+
+	// TODO : required http header must be added here.
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.New("unable to get access token")
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return "", errors.New("failed on create access token")
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", fmt.Errorf("unable to read response body : %v", err)
+	}
+
+	var data map[string]interface{}
+
+	if err = json.Unmarshal(body, &data); err != nil {
+		return "", errors.New("unable to unmarshal response body")
+	}
+
+	token, ok := data[AccessTokenKey]
+	if !ok {
+		return "", errors.New("key for access token does not exist in response body")
+	}
+	return AccessToken(fmt.Sprint(token)), nil
 }
--- client-go/auth_test.go
+++ client-go/auth_test.go
@@ -1,7 +1,7 @@
 package clientgo_test
 
 import (
-	"log"
+	"os"
 	"testing"
 
 	clientgo "github.com/xjayleex/sodas-sdk-go/client-go"
@@ -9,18 +9,35 @@ import (
 )
 
 var (
-	authArgs            = testutil.DataLakeAuthDefaultArgs
+	authArgs            = testutil.DataLakeAuthDefaultConfig
 	refreshTokenRequest *clientgo.RefreshTokenRequest
+	refreshToken        clientgo.RefreshToken
+	accessToken         clientgo.AccessToken
 )
 
 func TestMain(m *testing.M) {
-	refreshTokenRequest = clientgo.NewRefreshTokenRequest()
+	refreshTokenRequest = clientgo.NewRefreshTokenRequest(authArgs["base_url"], authArgs["user_name"], authArgs["password"])
+	ret := m.Run()
+	os.Exit(ret)
 }
 
 func TestGetRefreshToken(t *testing.T) {
-	_, err := clientgo.GetRefreshToken(refreshTokenRequest)
+	var err error
+	refreshToken, err = clientgo.GetRefreshToken(refreshTokenRequest)
 	if err != nil {
-		log.Println(err)
+		t.Log(err)
+	} else {
+		t.Log(refreshToken)
 	}
+}
 
+func TestGetAccessToken(t *testing.T) {
+	var err error
+	template := clientgo.NewAccessTokenRequest(authArgs["base_url"], authArgs["user_name"], &refreshToken)
+	accessToken, err = clientgo.GetAccessToken(template)
+	if err != nil {
+		t.Log(err)
+	} else {
+		t.Log(accessToken)
+	}
 }
